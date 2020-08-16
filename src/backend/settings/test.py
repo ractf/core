0055bed21a7b0ce1b97c99654debcca9a805f931
Jeremiah Boby <mail@jeremiahboby.me>
2020-08-16 13:28:41 +0100

    Add support for a self-contained testing environment

---
+++ src/backend/settings/test.py
@@ -0,0 +1,28 @@
+from . import *
+
+
+SECRET_KEY = "CorrectHorseBatteryStaple"
+
+MAIL["SEND"] = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+
+CONFIG = {
+    "BACKEND": "config.backends.DatabaseBackend",
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'dead-beef',
+    }
+}
