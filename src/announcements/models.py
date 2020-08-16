c61d0b5984c8a35631816ec11e10ff86649db42a
David Cooke <cookedavid637@gmail.com>
2020-08-16 19:48:59 +0100

    Permissions don't need defining

--- src/announcements/models.py
+++ src/announcements/models.py
@@ -6,10 +6,3 @@ class Announcement(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        permissions = [
-            ('add_announcement', 'add_announcement'),
-            ('change_announcement', 'change_announcement'),
-            ('delete_announcement', 'delete_announcement'),
-        ]
