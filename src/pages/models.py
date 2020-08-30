ac5849fde8acc6446dc9e7b537a2ff44767e0223
BenTechy66 <sendbenspam@yahoo.co.uk>
2020-08-30 18:51:29 +0100

    do that thing botters wanted

--- src/pages/models.py
+++ src/pages/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    url = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, unique=True)
--- src/pages/views.py
+++ src/pages/views.py
@@ -2,12 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from pages.models import Page
 from pages.serializers import PageSerializer
-from backend.permissions import AdminOrReadOnly
+from backend.permissions import AdminOrAnonymousReadOnly
 
 
 class TagViewSet(ModelViewSet):
     queryset = Page.objects.all()
-    permission_classes = (AdminOrReadOnly,)
+    permission_classes = (AdminOrAnonymousReadOnly,)
     throttle_scope = 'pages'
     serializer_class = PageSerializer
     pagination_class = None
