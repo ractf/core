33fd548af92ea36215a502445d61a2fd62704184
David Cooke <cookedavid637@gmail.com>
2020-09-13 17:59:03 +0100

    fix hint serializer

--- src/challenge/serializers.py
+++ src/challenge/serializers.py
@@ -15,6 +15,29 @@ class NestedTagSerializer(serializers.ModelSerializer):
         fields = ['text', 'type']
 
 
+
+
+
+class HintSerializer(serializers.ModelSerializer):
+    text = serializers.SerializerMethodField()
+    used = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Hint
+        fields = ["id", "name", "penalty", "challenge", "text", "used"]
+
+    def get_text(self, instance):
+        if (
+            self.context["request"].user.is_staff
+            and not self.context["request"].user.should_deny_admin()
+        ) or instance.used:
+            return instance.text
+        else:
+            return ""
+
+    def get_used(self, instance):
+        return instance.used
+
 class ChallengeSerializerMixin:
     def get_unlocked(self, instance):
         return instance.unlocked
@@ -197,27 +220,6 @@ class HintUseSerializer(serializers.ModelSerializer):
     class Meta:
         model = HintUse
         fields = ["id", "hint", "team", "user", "timestamp"]
-
-
-class HintSerializer(serializers.ModelSerializer):
-    text = serializers.SerializerMethodField()
-    used = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Hint
-        fields = ["id", "name", "penalty", "challenge", "text", "used"]
-
-    def get_text(self, instance):
-        if (
-            self.context["request"].user.is_staff
-            and not self.context["request"].user.should_deny_admin()
-        ) or instance.used:
-            return instance.text
-        else:
-            return ""
-
-    def get_used(self, instance):
-        return instance.used
 
 
 class CreateHintSerializer(serializers.ModelSerializer):
