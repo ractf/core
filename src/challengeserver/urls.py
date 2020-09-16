726509ba89d58a9a664372c67a28b89d414b2a02
David Cooke <cookedavid637@gmail.com>
2020-09-16 23:08:46 +0100

    andromeda job submit endpoints

--- src/challengeserver/urls.py
+++ src/challengeserver/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('instances/', views.ListInstancesView.as_view(), name='list-instances'),
     path('sysinfo/', views.SysinfoView.as_view(), name='view-sysinfo'),
     path('submit_job', views.JobSubmitView.as_view(), name='submit-job'),
-    path('submit_job_raw', views.JobSubmitView.as_view(), name='submit-job'),
+    path('submit_job_raw', views.JobSubmitRawView.as_view(), name='submit-job'),
 ]
