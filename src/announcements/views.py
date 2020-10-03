9c1c55ed091193a6bdd2a66d39f42fd4d158230d
Josh Heng <josh@joshheng.co.uk>
2020-10-03 15:17:34 +0100

    Document announcement views

--- src/announcements/views.py
+++ src/announcements/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.schemas.openapi import AutoSchema
 
 from announcements.models import Announcement
 from announcements.serializers import AnnouncementSerializer
@@ -6,6 +7,26 @@ from backend.permissions import AdminOrReadOnly
 
 
 class AnnouncementViewSet(ModelViewSet):
+    """
+    list:
+    Retrieve all announcements.
+
+    create:
+    Create a new announcement.
+
+    retrieve:
+    Retrieve a specific announcement.
+
+    update:
+    Update an announcement.
+
+    partial_update:
+    Partially update an announcement.
+
+    destroy:
+    Delete an announcement.
+    """
+
     queryset = Announcement.objects.all()
     permission_classes = (AdminOrReadOnly,)
     throttle_scope = "announcement"
