c0ae028bf429638309f3c6155f054d7fc8eacd18
bentechy <sendbenspam@yahoo.co.uk>
2021-01-11 19:38:36 +0000

    expose pagination counts

--- src/backend/pagination.py
+++ src/backend/pagination.py
@@ -10,7 +10,8 @@ class FastPagination(LimitOffsetPagination):
         return FormattedResponse(OrderedDict([
             ('next', self.format_link(self.get_next_link())),
             ('previous', self.format_link(self.get_previous_link())),
-            ('results', data)
+            ('results', data),
+            ('count', self.count)
         ]))
 
     @staticmethod
