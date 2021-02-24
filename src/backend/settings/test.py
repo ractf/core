e9bca640f2d6cf8c761d131afacfdea2619aca45
Daniel Milnes <daniel.milnes@lmax.com>
2021-02-24 14:01:15 +0000

    Don't override DB and Cache in CI

--- src/backend/settings/test.py
+++ src/backend/settings/test.py
@@ -5,24 +5,24 @@ SECRET_KEY = "CorrectHorseBatteryStaple"
 
 MAIL["SEND"] = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'db.sqlite3',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#    },
+#}
 
 CONFIG = {
     "BACKEND": "config.backends.DatabaseBackend",
 }
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'dead-beef',
-    }
-}
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#        'LOCATION': 'dead-beef',
+#    }
+#}
