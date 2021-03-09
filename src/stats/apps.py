d50947646cb57a3f16344468d491cbc9948ebf91
Jeremiah Boby <mail@jeremiahboby.me>
2021-03-09 20:18:09 +0000

    Update stats/apps.py

--- src/stats/apps.py
+++ src/stats/apps.py
@@ -1,5 +1,7 @@
-from django.apps import AppConfig
-from django.db.models.signals import post_save
+import sys
+
+from django.apps import AppConfig, apps
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from prometheus_client import Gauge
@@ -11,9 +13,16 @@ class StatsConfig(AppConfig):
     name = "stats"
 
     def ready(self):
-        from member.models import Member
-        from team.models import Team
-        from challenge.models import Solve
+        """Logic for adding extra prometheus statistics."""
+
+        if "migrate" in sys.argv or "makemigrations" in sys.argv:
+            # Don't run stats-related logic if we haven't migrated yet
+            return
+
+        # Pull relevant models from the other apps
+        Team = apps.get_model("team", "Team")
+        Member = apps.get_model("member", "Member")
+        Solve = apps.get_model("challenge", "Solve")
 
         member_count = Gauge("member_count", "The number of members currently registered")
         member_count.set(Member.objects.count())
@@ -27,17 +36,27 @@ class StatsConfig(AppConfig):
         correct_solve_count = Gauge("correct_solve_count", "The count of correct solves")
         correct_solve_count.set(Solve.objects.filter(correct=True).count())
 
-        connected_websocket_users = Gauge("connected_websocket_users", "The number of users connected to the Websocket")
+        connected_websocket_users = Gauge(
+            "connected_websocket_users", "The number of users connected to the Websocket"
+        )
 
         @receiver(post_save, sender=Member)
         def on_member_create(sender, instance, created, **kwargs):
             if created:
                 member_count.inc()
 
+        @receiver(post_delete, sender=Member)
+        def on_member_delete(sender, instance, **kwargs):
+            member_count.dec()
+
         @receiver(post_save, sender=Team)
         def on_team_create(sender, instance, created, **kwargs):
             if created:
                 team_count.inc()
+
+        @receiver(post_save, sender=Team)
+        def on_team_delete(sender, instance, **kwargs):
+            team_count.dec()
 
         @receiver(post_save, sender=Solve)
         def on_solve_create(sender, instance, created, **kwargs):
@@ -45,6 +64,12 @@ class StatsConfig(AppConfig):
                 solve_count.inc()
                 if instance.correct:
                     correct_solve_count.inc()
+
+        @receiver(post_save, sender=Solve)
+        def on_solve_delete(sender, instance, **kwargs):
+            solve_count.dec()
+            if instance.correct:
+                correct_solve_count.dec()
 
         @receiver(websocket_connect)
         def on_ws_connect():
