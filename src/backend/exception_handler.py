f3df1f25f86107fe2f83b18f00838250741ae4c9
David Cooke <me@dave.lc>
2021-04-09 03:27:27 +0100

    add a docstring to the exception handler

--- src/backend/exception_handler.py
+++ src/backend/exception_handler.py
@@ -13,6 +13,18 @@ from backend.response import FormattedResponse
 
 
 def handle_exception(exc: Exception, context: dict) -> Response:
+    """
+    Attempts to fit exceptions into the SMD format. If the X-Reasonable header is included, the exception is handled
+    solely by the default handler. Instances of FormattedException will be separated out into its provided values and
+    returned early. The exception is first handled by the default handler to get a base response, then if the exception
+    is an instance of Http404 or PermissionDenied, a simple response will be returned with the message "not_found" or
+    "permission_denied" respectively. If the exception is an instance of APIException and the detail attribute is a
+    list, the code of the first element of the list will be returned in the m field, and the whole list will be included
+    in the d field, if the detail attribute is a dict, the dict is iterated then the code of the first item is returned
+    in m and the whole dict is returned in d, if the detail is a single item, the code is returned in m and d is
+    omitted. If the exception is something else, the status code is set to 500, and the m field is set to
+    "internal_server_error" and the d field is set to the string value of the exception.
+    """
     if "X-Reasonable" in context['request'].headers:
         return exception_handler(exc, context)
     if settings.DEBUG:
@@ -36,8 +48,9 @@ def handle_exception(exc: Exception, context: dict) -> Response:
         else:
             response.data = {"s": False, "m": exc.detail.code, "d": ""}
     else:
+        traceback.print_exc()
         response = Response(
-            {"s": False, "m": str(exc), "d": ""}, status=HTTP_500_INTERNAL_SERVER_ERROR
+            {"s": False, "m": "internal_server_error", "d": str(exc)}, status=HTTP_500_INTERNAL_SERVER_ERROR
         )
 
     return response
