a8051ed0a175297601af036aa49aecbc79db7a21
David Cooke <me@dave.lc>
2021-04-12 05:26:32 +0100

    disable rate limits for testing

--- src/backend/settings/test.py
+++ src/backend/settings/test.py
@@ -8,6 +8,9 @@ MAIL["SEND"] = False
 FRONTEND_URL = "http://example.com/"
 DOMAIN = "example.com"
 
+for scope in REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"]:
+    REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"][scope] = "9999999/minute"
+
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
