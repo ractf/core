5073b7bb63f8ed47d83c5c64a004d28e0e25ec31
Jeremiah Boby <mail@jeremiahboby.me>
2021-04-24 15:24:24 +0100

    Send unhandled exceptions to Sentry

--- src/backend/exception_handler.py
+++ src/backend/exception_handler.py
@@ -1,29 +1,38 @@
 import traceback
+from typing import Optional
 
 from django.http import Http404
+
 from rest_framework import exceptions
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.views import exception_handler
 
+import sentry_sdk
+
 from django.conf import settings
 from backend.exceptions import FormattedException
 from backend.response import FormattedResponse
 
 
-def handle_exception(exc, context):
-    if "X-Reasonable" in context['request'].headers:
+def handle_exception(exc: Exception, context: dict) -> Optional[Response]:
+    """Handle exceptions, sending data to Sentry if errors are unhandled."""
+
+    if "X-Reasonable" in context["request"].headers:
         return exception_handler(exc, context)
-    if settings.DEBUG:
-        traceback.print_exc()
+
     if isinstance(exc, FormattedException):
         return FormattedResponse(s=False, d=exc.d, m=exc.m, status=exc.status_code)
+
     response = exception_handler(exc, context)
+
     if isinstance(exc, Http404):
         response.data = {"s": False, "m": "not_found", "d": ""}
+
     elif isinstance(exc, PermissionDenied):
         response.data = {"s": False, "m": "permission_denied", "d": ""}
+
     elif isinstance(exc, exceptions.APIException):
         if isinstance(exc.detail, list):
             response.data = {"s": False, "m": exc.detail[0].code, "d": exc.detail}
@@ -35,9 +44,11 @@ def handle_exception(exc, context):
             response.data = {"s": False, "m": errors[0], "d": exc.detail}
         else:
             response.data = {"s": False, "m": exc.detail.code, "d": ""}
+
     else:
         response = Response(
-            {"s": False, "m": str(exc), "d": ""}, status=HTTP_500_INTERNAL_SERVER_ERROR
+            {"s": False, "m": "Internal server error.", "d": ""},
+            status=HTTP_500_INTERNAL_SERVER_ERROR,
         )
-
+        sentry_sdk.capture_exception(exc)
     return response
