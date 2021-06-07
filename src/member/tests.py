132c0fbf7c9c76b98036218efce8fb079dea5e1a
Jeremiah Boby <mail@jeremiahboby.me>
2021-06-07 22:13:34 +0100

    Remove redundant assignments of 'response'

--- src/member/tests.py
+++ src/member/tests.py
@@ -43,7 +43,7 @@ class MemberTestCase(APITestCase):
         pr_token = self.user.password_reset_token
         ev_token = self.user.email_token
         self.client.force_authenticate(self.user)
-        response = self.client.put(reverse("member-self"), data={"email": "test-self3@example.org"})
+        self.client.put(reverse("member-self"), data={"email": "test-self3@example.org"})
         user = get_user_model().objects.get(id=self.user.id)
         self.assertNotEqual(pr_token, user.password_reset_token)
         self.assertNotEqual(ev_token, user.email_token)
