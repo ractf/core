0d282ac0747f9b94c6f23b7c95e298667c4431bf
David <me@dave.lc>
2021-06-12 00:45:34 +0100

    docstrings on hint serializers

--- src/hint/serializers.py
+++ src/hint/serializers.py
@@ -1,3 +1,5 @@
+"""Serializers for the hint app."""
+
 import serpy
 from rest_framework import serializers
 
@@ -5,6 +7,7 @@ from hint.models import Hint, HintUse
 
 
 def is_used(context, instance):
+    """Return True if a user can view a hint."""
     return (
         context["request"].user.team is not None
         and context["request"].user.team.hints_used.filter(hint=instance).exists()
@@ -12,13 +15,20 @@ def is_used(context, instance):
 
 
 class HintUseSerializer(serializers.ModelSerializer):
+    """Serializer for a hint use."""
+
     class Meta:
+        """The fields that should be serialized for a hint use."""
+
         model = HintUse
         fields = ["id", "hint", "team", "user", "timestamp"]
 
 
 class HintSerializerMixin:
+    """Common functions for hint-related serializers."""
+
     def get_text(self, instance):
+        """Get the text of a hint or an empty string if the hint is locked."""
         if (self.context["request"].user.is_staff and not self.context["request"].user.should_deny_admin()) or is_used(
             self.context, instance
         ):
@@ -27,19 +37,26 @@ class HintSerializerMixin:
             return ""
 
     def get_used(self, instance):
+        """Return True if the hint is used."""
         return is_used(self.context, instance)
 
 
 class HintSerializer(serializers.ModelSerializer, HintSerializerMixin):
+    """Serializer for the Hint model."""
+
     text = serializers.SerializerMethodField()
     used = serializers.SerializerMethodField()
 
     class Meta:
+        """The fields that should be serialized."""
+
         model = Hint
         fields = ["id", "name", "penalty", "challenge", "text", "used"]
 
 
 class FastHintSerializer(serpy.Serializer):
+    """Serpy serializer for hints."""
+
     id = serpy.IntField()
     name = serpy.StrField()
     penalty = serpy.IntField()
@@ -48,22 +65,33 @@ class FastHintSerializer(serpy.Serializer):
 
 
 class CreateHintSerializer(serializers.ModelSerializer):
+    """Serializer for creating hints."""
+
     class Meta:
+        """The fields that should be serialized."""
+
         model = Hint
         fields = ["id", "name", "penalty", "challenge", "text"]
         read_only_fields = ["id"]
 
 
 class FullHintSerializer(serializers.ModelSerializer):
+    """Serializer for the full details of a hint."""
+
     used = serializers.SerializerMethodField()
 
     class Meta:
+        """The fields that should be serialized."""
+
         model = Hint
         fields = ["id", "name", "penalty", "challenge", "text", "used"]
 
     def get_used(self, instance):
+        """Return True if the hint is used."""
         return is_used(self.context, instance)
 
 
 class UseHintSerializer(serializers.Serializer):
+    """Serializer for the HintUse model."""
+
     id = serializers.IntegerField()
