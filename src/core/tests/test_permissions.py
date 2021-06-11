a539082ca9767b62381cd2c53b4ce97aa408af32
David <me@dave.lc>
2021-06-11 16:11:04 +0100

    Test AdminOrReadOnlyVisible

--- src/core/tests/test_permissions.py
+++ src/core/tests/test_permissions.py
@@ -2,19 +2,64 @@ from django.http import HttpRequest
 from rest_framework.request import Request
 from rest_framework.test import APITestCase
 
-from core.permissions import ReadOnlyBot
+from core.permissions import AdminOrReadOnlyVisible, ReadOnlyBot
 from member.models import Member
 
 
-class ReadOnlyBotTestCase(APITestCase):
-    def test_is_bot_safe_method(self):
+class PermissionTestMixin:
+    def create_request(self, method: str) -> Request:
         request = Request(HttpRequest())
-        request.method = "GET"
+        request.method = method
+        return request
+
+
+class ReadOnlyBotTestCase(PermissionTestMixin, APITestCase):
+    def test_is_bot_safe_method(self) -> None:
+        request = self.create_request("GET")
         request.user = Member(username="bot-test", email="bot-test@gmail.com", is_bot=True)
         self.assertTrue(ReadOnlyBot().has_permission(request, None))
 
-    def test_is_bot_unsafe_method(self):
-        request = Request(HttpRequest())
-        request.method = "POST"
+    def test_is_bot_unsafe_method(self) -> None:
+        request = self.create_request("POST")
         request.user = Member(username="bot-test", email="bot-test@gmail.com", is_bot=True)
         self.assertFalse(ReadOnlyBot().has_permission(request, None))
+
+
+class AdminOrReadOnlyVisibleTestCase(PermissionTestMixin, APITestCase):
+    def test_admin_safe(self):
+        request = self.create_request("GET")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com", is_staff=True)
+        self.assertTrue(AdminOrReadOnlyVisible().has_object_permission(request, None, None))
+
+    def test_admin_unsafe(self):
+        request = self.create_request("POST")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com", is_staff=True)
+        self.assertTrue(AdminOrReadOnlyVisible().has_object_permission(request, None, None))
+
+    def test_not_admin_safe_visible(self):
+        request = self.create_request("GET")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com")
+        obj = type("Object", (object,), {})
+        obj.is_visible = True
+        self.assertTrue(AdminOrReadOnlyVisible().has_object_permission(request, None, obj))
+
+    def test_not_admin_unsafe_visible(self):
+        request = self.create_request("POST")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com")
+        obj = type("Object", (object,), {})
+        obj.is_visible = True
+        self.assertFalse(AdminOrReadOnlyVisible().has_object_permission(request, None, obj))
+
+    def test_not_admin_safe_not_visible(self):
+        request = self.create_request("GET")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com")
+        obj = type("Object", (object,), {})
+        obj.is_visible = False
+        self.assertFalse(AdminOrReadOnlyVisible().has_object_permission(request, None, obj))
+
+    def test_not_admin_unsafe_not_visible(self):
+        request = self.create_request("POST")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com")
+        obj = type("Object", (object,), {})
+        obj.is_visible = False
+        self.assertFalse(AdminOrReadOnlyVisible().has_object_permission(request, None, obj))
