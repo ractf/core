2b9e3bb10904028fc04078cdfaff5f50a40439d9
David <me@dave.lc>
2021-06-11 18:49:50 +0100

    challenge app docstrings.

--- src/challenge/apps.py
+++ src/challenge/apps.py
@@ -1,10 +1,15 @@
+"""App for managing challenges."""
+
 from importlib import import_module
 
 from django.apps import AppConfig
 
 
 class ChallengeConfig(AppConfig):
+    """The app config for the challenge app."""
+
     name = "challenge"
 
     def ready(self):
+        """Import challenge signals when the app is ready."""
         import_module("challenge.signals", "challenge")
