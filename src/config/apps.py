5dabf0fc44e207b41f67384cc571b1330587873f
David <me@dave.lc>
2021-06-12 00:56:49 +0100

    config app docstrings

--- src/config/apps.py
+++ src/config/apps.py
@@ -1,10 +1,15 @@
+"""App managing the backend config."""
+
 from django.apps import AppConfig
 
 from config import config
 
 
 class ConfigConfig(AppConfig):
+    """The app config for the config app."""
+
     name = "config"
 
     def ready(self) -> None:
+        """Load the config when django is ready."""
         config.load()
