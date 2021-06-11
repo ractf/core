cc6c41b9f2f625fc72f33c2811d53adacaed6d78
David <me@dave.lc>
2021-06-11 19:09:35 +0100

    challenge permission docstrings.

--- src/challenge/permissions.py
+++ src/challenge/permissions.py
@@ -1,3 +1,5 @@
+"""Permissions for the challenge app."""
+
 import time
 
 from rest_framework import permissions
@@ -6,7 +8,10 @@ from config import config
 
 
 class CompetitionOpen(permissions.BasePermission):
+    """Permission that checks if the competition is open."""
+
     def has_permission(self, request, view):
+        """Return if the competition is open or the action should be allowed anyway."""
         return (request.user.is_staff and not request.user.should_deny_admin()) or (
             config.get("start_time") <= time.time()
             and (config.get("enable_view_challenges_after_competion") or time.time() <= config.get("end_time"))
