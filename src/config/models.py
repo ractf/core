d248c234525313f9472ceeb5dc6dbb99c10f4049
David <me@dave.lc>
2021-06-12 00:52:47 +0100

    config model docstrings

--- src/config/models.py
+++ src/config/models.py
@@ -1,7 +1,11 @@
+"""Database models for the config app."""
+
 from django.db import models
 from django_prometheus.models import ExportModelOperationsMixin
 
 
 class Config(ExportModelOperationsMixin("config"), models.Model):
+    """Represents the persisted version of the backed config."""
+
     key = models.CharField(max_length=64, unique=True)
     value = models.JSONField()
