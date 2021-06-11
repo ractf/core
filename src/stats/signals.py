2eb59b593095c9e224085806a981fa95a51baf51
David <me@dave.lc>
2021-06-11 17:35:18 +0100

    add docstrings to stats signal handlers

--- src/stats/signals.py
+++ src/stats/signals.py
@@ -1,3 +1,5 @@
+"""Signal handlers and prometheus gauges for the stats app."""
+
 from django.core.cache import cache
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
@@ -38,26 +40,31 @@ if not cache.get("migrations_needed"):
 
 @receiver(register)
 def on_member_create(sender, user, **kwargs):
+    """Increment the member_count gauge."""
     cache.set("member_count", cache.get("member_count") + 1, timeout=None)
 
 
 @receiver(post_delete, sender=Member)
 def on_member_delete(sender, instance, **kwargs):
+    """Decrement the member_count gauge."""
     cache.set("member_count", cache.get("member_count") - 1, timeout=None)
 
 
 @receiver(team_create)
 def on_team_create(sender, team, **kwargs):
+    """Increment the team_count gauge."""
     cache.set("team_count", cache.get("team_count") + 1, timeout=None)
 
 
 @receiver(post_delete, sender=Team)
 def on_team_delete(sender, instance, **kwargs):
+    """Decrement the team_count gauge."""
     cache.set("team_count", cache.get("team_count") - 1, timeout=None)
 
 
 @receiver(flag_score)
 def on_solve_create(sender, user, team, challenge, flag, solve, **kwargs):
+    """Increment the solve_count gauge, and if the solve is correct, the correct_solve_count gauge."""
     cache.set("solve_count", cache.get("solve_count") + 1, timeout=None)
     if solve.correct:
         cache.set("correct_solve_count", cache.get("correct_solve_count") + 1, timeout=None)
@@ -65,6 +72,7 @@ def on_solve_create(sender, user, team, challenge, flag, solve, **kwargs):
 
 @receiver(post_delete, sender=Solve)
 def on_solve_delete(sender, instance, **kwargs):
+    """Decrement the solve_count gauge, and if the solve is correct, the correct_solve_count gauge."""
     cache.set("solve_count", cache.get("solve_count") - 1, timeout=None)
     if instance.correct:
         cache.set("correct_solve_count", cache.get("correct_solve_count") - 1, timeout=None)
@@ -72,9 +80,11 @@ def on_solve_delete(sender, instance, **kwargs):
 
 @receiver(websocket_connect)
 def on_ws_connect(**kwargs):
+    """Increment the websocket connections gauge."""
     connected_websocket_users.inc()
 
 
 @receiver(websocket_disconnect)
 def on_ws_disconnect(**kwargs):
+    """Decrement the websocket connections gauge."""
     connected_websocket_users.dec()
