b561b471e39912ef9fd8dc34584a5b0190b75224
David <me@dave.lc>
2021-06-12 00:25:38 +0100

    docstrings on hint models

--- src/hint/models.py
+++ src/hint/models.py
@@ -1,13 +1,14 @@
+"""Models for the hint app."""
+
 from django.db import models
 from django.db.models import CASCADE, SET_NULL
 from django.utils import timezone
 from django_prometheus.models import ExportModelOperationsMixin
 
-from challenge.models import Challenge
-from team.models import Team
-
 
 class Hint(ExportModelOperationsMixin("hint"), models.Model):
+    """Represents a hint for a challenge."""
+
     name = models.CharField(max_length=36)
     challenge = models.ForeignKey("challenge.Challenge", related_name="hint_set", on_delete=CASCADE)
     text = models.TextField()
@@ -15,6 +16,8 @@ class Hint(ExportModelOperationsMixin("hint"), models.Model):
 
 
 class HintUse(ExportModelOperationsMixin("hint_use"), models.Model):
+    """Represents a user/team redeeming a hint."""
+
     hint = models.ForeignKey("hint.Hint", related_name="uses", on_delete=CASCADE)
     team = models.ForeignKey("team.Team", related_name="hints_used", on_delete=CASCADE, null=True)
     user = models.ForeignKey("member.Member", related_name="hints_used", on_delete=SET_NULL, null=True)
@@ -22,6 +25,8 @@ class HintUse(ExportModelOperationsMixin("hint_use"), models.Model):
     challenge = models.ForeignKey("challenge.Challenge", related_name="hints_used", on_delete=CASCADE)
 
     class Meta:
+        """The constraints on the model."""
+
         unique_together = (
             "hint",
             "team",
