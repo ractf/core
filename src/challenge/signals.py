b5333cd37bf1b092d245e35dbc9feb0648026f79
David <me@dave.lc>
2021-06-11 19:08:26 +0100

    challenge signal docstrings.

--- src/challenge/signals.py
+++ src/challenge/signals.py
@@ -1,3 +1,5 @@
+"""Signal receivers for the challenge app."""
+
 from django.core.cache import caches
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -7,5 +9,6 @@ from challenge.models import Challenge
 
 @receiver(post_save, sender=Challenge)
 def challenge_save(sender, instance, **kwargs):
+    """Update the challenge modification index when a challenge is modified."""
     new_index = caches["default"].get("challenge_mod_index", 0) + 1
     caches["default"].set("challenge_mod_index", new_index)
