52c0ad606661377820eb660364065ac12679ab9f
David <me@dave.lc>
2021-06-11 17:58:22 +0100

    challenge util test docstringas.

--- src/challenge/tests/test_utils.py
+++ src/challenge/tests/test_utils.py
@@ -1,3 +1,5 @@
+"""Unit tests for challenge utils"""
+
 from unittest import TestCase
 
 from django.contrib.auth import get_user_model
@@ -9,13 +11,19 @@ from config import config
 
 
 class CacheKeyTestCase(TestCase):
+    """Tests for cache key generation."""
+
     def test_get_cache_key_no_team(self):
+        """Test the cache key generates correctly with no team."""
         user = get_user_model()(username="cachekeytest", email="cachekeytest@example.com")
         self.assertTrue(get_cache_key(user).endswith("no_team"))
 
 
 class SqlTestCase(APITestCase):
+    """Tests for the raw sql."""
+
     def test_get_positive_votes_cached(self):
+        """Test the positive votes are correctly cached"""
         config.set("enable_caching", True)
         first = get_positive_votes()
         second = get_positive_votes()
@@ -23,6 +31,7 @@ class SqlTestCase(APITestCase):
         self.assertEqual(first, second)
 
     def test_get_negative_votes_cached(self):
+        """Test the negative votes are correctly cached"""
         config.set("enable_caching", True)
         first = get_negative_votes()
         second = get_negative_votes()
