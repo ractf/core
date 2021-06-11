a86a38a055994897ec8f050475d165aa5daa247a
David <me@dave.lc>
2021-06-11 16:16:10 +0100

    Test AdminOrReadOnly

--- src/core/tests/test_permissions.py
+++ src/core/tests/test_permissions.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest
 from rest_framework.request import Request
 from rest_framework.test import APITestCase
 
-from core.permissions import AdminOrReadOnlyVisible, ReadOnlyBot
+from core.permissions import AdminOrReadOnly, AdminOrReadOnlyVisible, ReadOnlyBot
 from member.models import Member
 
 
@@ -63,3 +63,29 @@ class AdminOrReadOnlyVisibleTestCase(PermissionTestMixin, APITestCase):
         obj = type("Object", (object,), {})
         obj.is_visible = False
         self.assertFalse(AdminOrReadOnlyVisible().has_object_permission(request, None, obj))
+
+
+class AdminOrReadOnlyTestCase(PermissionTestMixin, APITestCase):
+    def test_admin_safe(self):
+        request = self.create_request("GET")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com", is_staff=True)
+        self.assertTrue(AdminOrReadOnly().has_object_permission(request, None, None))
+
+    def test_admin_unsafe(self):
+        request = self.create_request("POST")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com", is_staff=True)
+        self.assertTrue(AdminOrReadOnly().has_object_permission(request, None, None))
+
+    def test_not_admin_safe(self):
+        request = self.create_request("GET")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com")
+        obj = type("Object", (object,), {})
+        obj.is_visible = True
+        self.assertTrue(AdminOrReadOnly().has_object_permission(request, None, obj))
+
+    def test_not_admin_unsafe(self):
+        request = self.create_request("POST")
+        request.user = Member(username="permission-test", email="permission-test@gmail.com")
+        obj = type("Object", (object,), {})
+        obj.is_visible = True
+        self.assertTrue(AdminOrReadOnly().has_object_permission(request, None, obj))
