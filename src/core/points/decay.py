6930ad09195f01aed402200f9db64c05f1d609ad
David <me@dave.lc>
2021-06-12 21:20:25 +0100

    decay pointplugin docstrings

--- src/core/points/decay.py
+++ src/core/points/decay.py
@@ -1,3 +1,5 @@
+"""Decay points plugin."""
+
 from django.contrib.auth import get_user_model
 from django.db.models import F
 
@@ -7,16 +9,28 @@ from core.points.base import PointsPlugin
 
 
 class DecayPointsPlugin(PointsPlugin):
+    """
+    Decay points plugin.
+
+    The amount of points decreases exponentially as more users solve the challenge.
+    """
+
     name = "decay"
     recalculate_type = "custom"
 
     def get_points(self, team, flag, solves, *args, **kwargs):
+        """Return the amount of points a solve is worth."""
         challenge = self.challenge
         decay_constant = challenge.challenge_metadata["decay_constant"]
         min_points = challenge.challenge_metadata["min_points"]
         return int(round(min_points + ((challenge.score - min_points) * (decay_constant ** max(solves - 1, 0)))))
 
     def recalculate(self, teams, users, solves, *args, **kwargs):
+        """
+        Recalculates the amount of points a solve is worth.
+
+        This will be called on every solve.
+        """
         challenge = self.challenge
         points = self.get_points(None, None, solves.count())
         delta = self.get_points(None, None, solves.count() - 1) - points
