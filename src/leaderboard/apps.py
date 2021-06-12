13a73ded9fc783e4b9fbb2742a4ae72075cae3db
David <me@dave.lc>
2021-06-12 18:15:16 +0100

    leaderboard app docstrings

--- src/leaderboard/apps.py
+++ src/leaderboard/apps.py
@@ -1,5 +1,9 @@
+"""App to display various scoreboards."""
+
 from django.apps import AppConfig
 
 
 class LeaderboardConfig(AppConfig):
+    """App config for the leaderboard app."""
+
     name = "leaderboard"
