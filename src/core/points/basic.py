71e50baec9a516c16e0146ebb13a10b38e31fcdb
David <me@dave.lc>
2021-06-12 21:17:32 +0100

    basic pointplugin docstrings

--- src/core/points/basic.py
+++ src/core/points/basic.py
@@ -1,8 +1,13 @@
+"""Basic points plugin."""
+
 from core.points.base import PointsPlugin
 
 
 class BasicPointsPlugin(PointsPlugin):
+    """Basic points plugin."""
+
     name = "basic"
 
     def get_points(self, team, flag, solves, *args, **kwargs):
+        """Return the challenge's points value."""
         return self.challenge.score
