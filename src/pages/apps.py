e2c471f8268e5684c5345d868334ecf1a81c6a7e
David <me@dave.lc>
2021-06-12 21:04:18 +0100

    page app docstrings

--- src/pages/apps.py
+++ src/pages/apps.py
@@ -1,5 +1,9 @@
+"""Apps for displaying custom pages on the frontend."""
+
 from django.apps import AppConfig
 
 
 class PagesConfig(AppConfig):
+    """App config for the page app."""
+
     name = "pages"
