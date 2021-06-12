4390e29a67b6bb7fc39142005029768bc17309e3
David <me@dave.lc>
2021-06-12 01:11:03 +0100

    config docstrings

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,5 @@
+"""Utility functions for other apps to access config."""
+
 from pydoc import locate
 
 from django.conf import settings
@@ -7,22 +9,27 @@ backend = SimpleLazyObject(lambda: locate(settings.CONFIG["BACKEND"])())
 
 
 def load():
+    """Load the config."""
     backend.load(defaults=settings.DEFAULT_CONFIG)
 
 
 def get(key):
+    """Get a config value."""
     return backend.get(key)
 
 
 def set(key, value):
+    """Set a config value."""
     backend.set(key, value)
 
 
 def get_all():
+    """Get all config values."""
     return backend.get_all()
 
 
 def get_all_non_sensitive():
+    """Get all non sensitive config values."""
     sensitive = backend.get("sensitive_fields")
     config = backend.get_all()
     for field in sensitive:
@@ -31,4 +38,5 @@ def get_all_non_sensitive():
 
 
 def is_sensitive(key):
+    """Return True if a config key is sensitive."""
     return key in backend.get("sensitive_fields")
