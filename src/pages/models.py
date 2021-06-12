8d809ca4371a051f95d4c14b2299421bfbd4fe84
David <me@dave.lc>
2021-06-12 21:02:13 +0100

    page model docstrings

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,8 +1,12 @@
+"""Database models for the pages app."""
+
 from django.db import models
 from django_prometheus.models import ExportModelOperationsMixin
 
 
 class Page(ExportModelOperationsMixin("page"), models.Model):
+    """Represents a custom page that will be displayed on the frontend."""
+
     title = models.CharField(max_length=255)
     content = models.TextField()
     url = models.CharField(max_length=255, unique=True)
