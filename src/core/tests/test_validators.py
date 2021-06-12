e1721d20b971316e1bbe019d71a77df0326a2b56
David <me@dave.lc>
2021-06-12 21:21:17 +0100

    validator test docstrings

--- src/core/tests/test_validators.py
+++ src/core/tests/test_validators.py
@@ -1,3 +1,5 @@
+"""Tests for validators used in core."""
+
 from django.core.exceptions import ValidationError
 from rest_framework.test import APITestCase
 
@@ -5,8 +7,12 @@ from core.validators import printable_name
 
 
 class PrintableNameValidatorTestCase(APITestCase):
+    """Tests for the printable name validator."""
+
     def test_unprintable_name(self):
+        """An unprintable name should be rejected."""
         self.assertRaises(ValidationError, lambda: printable_name(b"\x00".decode("latin-1")))
 
     def test_printable_name(self):
+        """A printable name should be accepted."""
         self.assertIsNone(printable_name("abc"))
