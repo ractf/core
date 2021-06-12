3465c77603ed90858fe390177cf0cb789253b088
David <me@dave.lc>
2021-06-12 21:11:08 +0100

    page serializer docstrings

--- src/pages/serializers.py
+++ src/pages/serializers.py
@@ -1,9 +1,15 @@
+"""Serializers for the page views."""
+
 from rest_framework import serializers
 
 from pages.models import Page
 
 
 class PageSerializer(serializers.ModelSerializer):
+    """Serializer for pages."""
+
     class Meta:
+        """The fields to serialize."""
+
         model = Page
         fields = ["id", "url", "title", "content"]
