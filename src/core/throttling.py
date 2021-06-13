b42a04279a1cb3014876be74f49f43be3ea35c3f
David <me@dave.lc>
2021-06-13 04:28:16 +0100

    core throttle docstrings

--- src/core/throttling.py
+++ src/core/throttling.py
@@ -1,9 +1,14 @@
+"""Custom throttling used in RACTF core."""
+
 from django.conf import settings
 from rest_framework import throttling
 
 
 class AdminBypassThrottle(throttling.ScopedRateThrottle):
+    """Subclass of DRF's ScopedRateThrottle to allow admins to bypass rate limits."""
+
     def allow_request(self, request, view):
+        """Return True if the user is admin or under the ratelimit."""
         if not settings.RATELIMIT_ENABLE:
             return True
         if request.user.is_staff and not request.user.should_deny_admin():
