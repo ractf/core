264480d907b1065287679d8a5a0b1625dc8e485e
David <me@dave.lc>
2021-06-13 05:03:05 +0100

    docstring sockets/models.py

--- src/sockets/models.py
+++ src/sockets/models.py
@@ -1,9 +1,13 @@
+"""Database models for the sockets app."""
+
 from django.db import models
 from django.utils import timezone
 from django_prometheus.models import ExportModelOperationsMixin
 
 
 class Announcement(ExportModelOperationsMixin("announcement"), models.Model):
+    """Represents an announcement show to users on the frontend."""
+
     title = models.CharField(max_length=255)
     body = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
