b417dfbbea8a010e1e65973b46ad85cf537c0a0f
David <me@dave.lc>
2021-06-13 04:17:39 +0100

    plugin base docstrings

--- src/core/base.py
+++ src/core/base.py
@@ -1,4 +1,7 @@
+"""Base class for plugins."""
+
+
 class Plugin:
-    """Base class used to identify plugins when loading them"""
+    """Base class used to identify plugins when loading them."""
 
     pass
