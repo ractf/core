4f9237c0d0594f9afbb89d6ec062021c105517f2
David <me@dave.lc>
2021-06-13 03:45:18 +0100

    add plaintext flag plugin docstrings

--- src/core/flag/plaintext.py
+++ src/core/flag/plaintext.py
@@ -1,14 +1,20 @@
+"""Plaintext flag plugin."""
+
 from config import config
 from core.flag.base import FlagPlugin
 
 
 class PlaintextFlagPlugin(FlagPlugin):
+    """Plaintext flag plugin."""
+
     name = "plaintext"
 
     def check(self, flag, *args, **kwargs):
+        """Return True if the flag matches the stored plaintext."""
         return self.challenge.flag_metadata["flag"] == flag
 
     def self_check(self):
+        """Ensure the set flag metadata has a 'flag' property and the flag matches the format."""
         if not self.challenge.flag_metadata.get("flag", ""):
             return ["property 'flag' must be set!"]
 
