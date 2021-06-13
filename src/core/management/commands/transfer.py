80c456711f2ccdb41070a4b5c2380fac3d41adea
David <me@dave.lc>
2021-06-13 03:49:13 +0100

    add transfer team docstrings

--- src/core/management/commands/transfer.py
+++ src/core/management/commands/transfer.py
@@ -1,3 +1,5 @@
+"""Command to transfer the owner of a team."""
+
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
@@ -5,13 +7,17 @@ from team.models import Team
 
 
 class Command(BaseCommand):
+    """Command to transfer the owner of a team."""
+
     help = "Transfer team owner"
 
     def add_arguments(self, parser):
+        """Add arguments to the parser."""
         parser.add_argument("user_id", type=int)
         parser.add_argument("team_id", type=int)
 
     def handle(self, *args, **options):
+        """Switch the owner of a team."""
         user = get_user_model().objects.get(pk=options["user_id"])
         team = Team.objects.get(pk=options["team_id"])
         team.owner = user
