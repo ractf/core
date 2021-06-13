7b31d4dcab5963a9e906ef60274178e20e287fb1
David <me@dave.lc>
2021-06-13 04:27:05 +0100

    core response docstrings

--- src/core/response.py
+++ src/core/response.py
@@ -1,10 +1,17 @@
+"""Common response formats used in RACTF core."""
+
 from rest_framework.response import Response
 
 
 class FormattedResponse(Response):
+    """A subclass of Response to attempt to make frontend's data format more reasonable to work with."""
+
+    # TODO: Deprecate this.
+
     def __init__(
         self, d="", m="", s=True, status=None, template_name=None, headers=None, exception=False, content_type=None
     ):
+        """Convert the success, data and message attributes to the data object."""
         if status and status >= 400:
             s = False
         data = {"s": s, "m": m, "d": d}
