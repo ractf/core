f9d0a5123647ab773a4c6521842eb63eea061edd
David <me@dave.lc>
2021-06-13 05:07:55 +0100

    docstring sockets/serializers.py

--- src/sockets/serializers.py
+++ src/sockets/serializers.py
@@ -1,9 +1,15 @@
+"""Serializers used for the sockets app."""
+
 from rest_framework.serializers import ModelSerializer
 
 from sockets.models import Announcement
 
 
 class AnnouncementSerializer(ModelSerializer):
+    """Serializer used for announcements."""
+
     class Meta:
+        """The fields to serialize."""
+
         model = Announcement
         fields = ["id", "body", "title", "timestamp"]
