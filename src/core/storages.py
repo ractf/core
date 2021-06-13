05c002cba1899e90a7aa9379ea42b213173018ed
David <me@dave.lc>
2021-06-13 04:29:04 +0100

    core storages docstrings

--- src/core/storages.py
+++ src/core/storages.py
@@ -1,6 +1,10 @@
+"""Storages used in RACTF core."""
+
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class PublicMediaStorage(S3Boto3Storage):
+    """S3 storage for challenge files."""
+
     location = "challenge-files"
     default_acl = None
