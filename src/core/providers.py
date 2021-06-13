1ca77ba3f5b7dbb0fd5a8f6bf2793c8509af86dd
David <me@dave.lc>
2021-06-13 04:57:42 +0100

    docstring core/providers.py

--- src/core/providers.py
+++ src/core/providers.py
@@ -1,3 +1,9 @@
+"""
+Module used for provider registration.
+
+A provider is a class that is capable of handling a request for a certain thing, such as logging in, registering, etc.
+"""
+
 import abc
 from collections import defaultdict
 
@@ -7,12 +13,16 @@ providers = defaultdict(dict)
 
 
 def register_provider(provider_type, provider):
+    """Register a provider."""
     providers[provider_type][provider.name] = provider
 
 
 def get_provider(provider_type):
+    """Get the selected provider for a provider type."""
     return providers[provider_type][config.get(provider_type + "_provider")]
 
 
 class Provider(abc.ABC):
+    """Base class all providers inherit from."""
+
     pass
