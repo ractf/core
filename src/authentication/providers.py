2a9af661322ac97ce8d756842b4d694c2a45c4fa
Jeremiah Boby <mail@jeremiahboby.me>
2021-06-13 05:07:28 +0100

    Add docstrings to authentication.providers

--- src/authentication/providers.py
+++ src/authentication/providers.py
@@ -4,50 +4,60 @@
 
 import abc
 
-from django.contrib.auth import get_user_model
 from django.core.validators import EmailValidator
+from django.db.models import Q
 from rest_framework.exceptions import ValidationError
 
 from config import config
 from core.providers import Provider
+from member.models import Member
 
 
 class RegistrationProvider(Provider, abc.ABC):  # pragma: no cover
+    """A Provider for user registration."""
+
     type = "registration"
 
     @abc.abstractmethod
     def validate(self, data):
+        """Validate the provided form data."""
         pass
 
     @abc.abstractmethod
     def register_user(self, **kwargs):
+        """Register the user once the form data passes validation."""
         pass
 
-    def validate_email(self, email):
+    def validate_email(self, email: str) -> None:
+        """Validate the email provided with Django's builtin validator."""
         allow_domain = config.get("email_allow")
-        if allow_domain:
-            email_validator = EmailValidator(allow_domain)
-        else:
-            email_validator = EmailValidator()
+        email_validator = EmailValidator(allow_domain or ...)
         if email_validator(email):
             raise ValidationError("invalid_email")
 
     def check_email_or_username_in_use(self, email=None, username=None):
-        if get_user_model().objects.filter(username=username) or get_user_model().objects.filter(email=email):
+        """Ensure that the provided email and username do not already exist."""
+        if Member.objects.filter(Q(username=username) | Q(email=email)):
             raise ValidationError("email_or_username_in_use")
 
 
 class LoginProvider(Provider, abc.ABC):  # pragma: no cover
+    """A Provider for user logins."""
+
     type = "login"
 
     @abc.abstractmethod
     def login_user(self, **kwargs):
+        """Athenticate this user with their session."""
         pass
 
 
 class TokenProvider(Provider, abc.ABC):  # pragma: no cover
+    """A Provider for token-based logins."""
+
     type = "token"
 
     @abc.abstractmethod
     def issue_token(self, user, **kwargs):
+        """Issue a token for this user."""
         pass
