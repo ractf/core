c921365bcad69bcc69ed57ffaef8d5e5b9812c62
David <me@dave.lc>
2021-06-13 05:10:05 +0100

    docstring sockets/consumers.py

--- src/sockets/consumers.py
+++ src/sockets/consumers.py
@@ -1,3 +1,5 @@
+"""Websocket consumers used by the sockets app."""
+
 import json
 
 from asgiref.sync import sync_to_async
@@ -8,25 +10,31 @@ from core.signals import websocket_connect, websocket_disconnect
 
 
 class EventConsumer(AsyncJsonWebsocketConsumer):
+    """The main websocket consumer for RACTF core."""
+
     groups = ["event"]
 
     async def connect(self):
+        """Handle a websocket connection."""
         await self.accept()
         await self.channel_layer.group_add("event", self.channel_name)
         await self.send_json({"event_code": 0, "message": "Websocket connected."})
         websocket_connect.send(self.__class__, channel_layer=self.channel_layer)
 
     async def disconnect(self, close_code):
+        """Handle a websocket disconnection."""
         websocket_disconnect.send(self.__class__, channel_layer=self.channel_layer)
 
     @staticmethod
     def get_team(token):
+        """Return a user's team."""
         if not Token.objects.filter(key=token).exists():
             return None
         user = Token.objects.get(key=token).user
         return user.team
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
+        """Handle an incoming websocket message."""
         try:
             data = json.loads(text_data)
         except json.decoder.JSONDecodeError:
