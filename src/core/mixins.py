815125415c96db00cc16585c3abe6fcd85ff6bc6
David <me@dave.lc>
2021-06-13 04:18:47 +0100

    mixin docstrings

--- src/core/mixins.py
+++ src/core/mixins.py
@@ -1,3 +1,9 @@
+"""Generic mixins used in RACTF core."""
+
+
 class IncorrectSolvesMixin:
+    """Mixin to add the incorrect_solves attribute to a serializer."""
+
     def get_incorrect_solves(self, instance):
+        """Return the count of incorrect solves."""
         return instance.solves.filter(correct=False).count()
