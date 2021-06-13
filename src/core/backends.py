d212126e0a793804a1acac3457eb9a83e31e161a
David <me@dave.lc>
2021-06-13 04:46:59 +0100

    docstring core/backends.py

--- src/core/backends.py
+++ src/core/backends.py
@@ -1,3 +1,5 @@
+"""Authentication backends define by RACTF core."""
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
@@ -5,11 +7,14 @@ UserModel = get_user_model()
 
 
 class EmailOrUsernameBackend(ModelBackend):
-    """
-    Authenticates against settings.AUTH_USER_MODEL.
-    """
+    """Authenticates against settings.AUTH_USER_MODEL."""
 
     def authenticate(self, request, username=None, password=None, **kwargs):
+        """
+        Return a user object if authentication is successful or None.
+
+        This is a copy of django's default authentication backend, except it uses the @ character to switch to email.
+        """
         try:
             if "@" in username:
                 user = UserModel.objects.get(email=username)
