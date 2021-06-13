06e6467052b79be2bf3dc55699f6f006e083829d
David <me@dave.lc>
2021-06-13 04:50:48 +0100

    docstring core/exceptions.py

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,9 +1,14 @@
+"""Exceptions defined by RACTF core."""
+
 from rest_framework.exceptions import APIException
 from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 
 
 class FormattedException(APIException):
+    """Used to throw an exception that fits into *that* format, because who doesnt like throwing away all useful info."""
+
     def __init__(self, d="", m="", status=HTTP_500_INTERNAL_SERVER_ERROR):
+        """Set the message and data attributes of a formatted response."""
         super(FormattedException, self).__init__()
         self.status_code = status
         self.m = m
