4020c2e2f54043c5cba8b1020611116fa9129174
David <me@dave.lc>
2021-06-13 05:03:59 +0100

    docstring sockets/apps.py

--- src/sockets/apps.py
+++ src/sockets/apps.py
@@ -1,10 +1,15 @@
+"""Configuration options and startup hooks for the sockets app."""
+
 from importlib import import_module
 
 from django.apps import AppConfig
 
 
 class SocketsConfig(AppConfig):
+    """App config for sockets."""
+
     name = "sockets"
 
     def ready(self):
+        """Load all signals when django is ready."""
         import_module("sockets.signals", "sockets")
