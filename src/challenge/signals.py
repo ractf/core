e4934597aab26fe1bd4d1d1fb71491714e0a03ac
David <me@dave.lc>
2021-07-21 20:28:56 +0100

    Invalidate challenge cache when a challenge is deleted.

--- src/challenge/signals.py
+++ src/challenge/signals.py
@@ -1,11 +1,17 @@
 from django.core.cache import caches
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from challenge.models import Challenge
 
 
 @receiver(post_save, sender=Challenge)
+def challenge_save(sender, instance, **kwargs):
+    new_index = caches["default"].get("challenge_mod_index", 0) + 1
+    caches["default"].set("challenge_mod_index", new_index)
+
+
+@receiver(post_delete, sender=Challenge)
 def challenge_save(sender, instance, **kwargs):
     new_index = caches["default"].get("challenge_mod_index", 0) + 1
     caches["default"].set("challenge_mod_index", new_index)
