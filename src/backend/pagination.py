d1c6eee1b3b66ece5a09cba316144cbc901a4f57
Jeremiah Boby <mail@jeremiahboby.me>
2021-08-02 17:57:32 +0100

    Add prepend_api_prefix method to FormattedPagination

--- src/backend/pagination.py
+++ src/backend/pagination.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from urllib import parse
+
 from collections import OrderedDict
 
 from rest_framework.pagination import LimitOffsetPagination
@@ -5,7 +8,21 @@ from rest_framework.pagination import LimitOffsetPagination
 from backend.response import FormattedResponse
 
 
+def prepend_api_prefix(url: Optional[str] = None) -> Optional[str]:
+    """Given a full URL as a string, prepend the /api/v2 prefix."""
+    if url is None:
+        return
+    result = parse.urlparse(url)
+    return f"{result.scheme}://{result.netloc}/api/v2{result.path}?{result.query}"
+
+
 class FormattedPagination(LimitOffsetPagination):
+    def get_next_link(self) -> Optional[str]:
+        return prepend_api_prefix(super().get_next_link())
+
+    def get_previous_link(self) -> Optional[str]:
+        return prepend_api_prefix(super().get_previous_link())
+
     def get_paginated_response(self, data):
         return FormattedResponse(
             OrderedDict(
