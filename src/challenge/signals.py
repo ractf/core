97511109939ad5611b50c8387a1f4d4e3d57b60c
David Cooke <me@dave.lc>
2021-08-07 19:41:27 +0100

    Invalidate challenge cache on hint file and tag changes

--- src/challenge/signals.py
+++ src/challenge/signals.py
@@ -2,11 +2,15 @@ from django.core.cache import caches
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from challenge.models import Category, Challenge
+from challenge.models import Category, Challenge, File, Tag
+from hint.models import Hint
 
 
 @receiver([post_save, post_delete], sender=Challenge)
 @receiver([post_save, post_delete], sender=Category)
+@receiver([post_save, post_delete], sender=Hint)
+@receiver([post_save, post_delete], sender=File)
+@receiver([post_save, post_delete], sender=Tag)
 def challenge_cache_invalidate(sender, instance, **kwargs):
     new_index = caches["default"].get("challenge_mod_index", 0) + 1
     caches["default"].set("challenge_mod_index", new_index, timeout=None)
