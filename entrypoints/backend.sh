be53c4d66344c52188f744cf8ce051e25407bb48
Jeremiah Boby <mail@jeremiahboby.me>
2021-08-18 20:59:21 +0100

    Update both compose services to use gunicorn
    Switches over the local docker-compose to use gunicorn workers for both
    the WSGI (core) and ASGI (sockets) servers. The latter uses the gunicorn
    worker provided by uvicorn. I've also added `GUNICORN_CMD_ARGS` to both
    entrypoint scripts to specify defaults for a gunicorn configuration.

--- entrypoints/backend.sh
+++ entrypoints/backend.sh
@@ -1,5 +1,4 @@
-#! /bin/sh
-
+#!/usr/bin/env sh
 
 if [ -z "$prometheus_multiproc_dir" ]
 then
@@ -9,18 +8,16 @@ else
   rm $prometheus_multiproc_dir/* -rf
 fi
 
-stdbuf -o 0 echo -n "Waiting for postgres... "
+echo "Waiting for postgres... "
 while ! nc -z $SQL_HOST $SQL_PORT
 do
   sleep 0.69
 done
 echo "Done."
 
-
 echo "Running migrations..."
 /app/src/manage.py migrate
 echo "Done."
-
 
 if [ "$LOAD_FIXTURES" ]
 then
@@ -28,10 +25,10 @@ then
   /app/src/manage.py loaddata test_fixtures
 fi
 
-
+export GUNICORN_CMD_ARGS="--chdir=/app/src/ --reload --workers=4 --bind=0.0.0.0:8000 --worker-class=gthread"
 if [ -f /etc/newrelic.ini ]
 then
   NEW_RELIC_CONFIG_FILE=/etc/newrelic.ini newrelic-admin run-program "$@"
 else
-  exec "$@"
+  exec $@
 fi
