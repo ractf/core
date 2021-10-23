fecd77987350b0c6ffd4c9a55c67e147af5e25c4
Daniel Milnes <thebeanogamer@gmail.com>
2021-10-23 15:38:36 +0100

    Automatically recalculate scores on save Resolves #200

--- src/challenge/signals.py
+++ src/challenge/signals.py
@@ -1,10 +1,12 @@
 from django.core.cache import caches
+from django.db import transaction
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from challenge.models import Category, Challenge, File, Tag
+from challenge.models import Category, Challenge, File, Score, Tag
 from challenge.views import get_cache_key
 from hint.models import Hint, HintUse
+from scorerecalculator.views import recalculate_team
 
 
 @receiver([post_save, post_delete], sender=Challenge)
@@ -15,6 +17,15 @@ from hint.models import Hint, HintUse
 def challenge_cache_invalidate(sender, instance, **kwargs):
     new_index = caches["default"].get("challenge_mod_index", 0) + 1
     caches["default"].set("challenge_mod_index", new_index, timeout=None)
+
+
+@receiver([post_save], sender=Challenge)
+def challenge_recalculate(sender, instance, **kwargs):
+    with transaction.atomic():
+        correct_solves = instance.solves.filter(correct=True)
+        Score.objects.filter(id__in=correct_solves.values_list("score", flat=True)).update(points=instance.score)
+        for solve in correct_solves:
+            recalculate_team(solve.team)
 
 
 @receiver([post_save, post_delete], sender=HintUse)
