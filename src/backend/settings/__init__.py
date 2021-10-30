feb2762baa9c1e5018d0909a4adb21b969ff6579
thebeanogamer <thebeanogamer@gmail.com>
2021-10-31 00:49:22 +0100

    Fix AWS variable name

--- src/backend/settings/__init__.py
+++ src/backend/settings/__init__.py
@@ -381,7 +381,7 @@ EMAIL_ENABLED = bool(os.getenv("EMAIL_PROVIDER", False))
 ANYMAIL = {
     "AMAZON_SES_CLIENT_PARAMS": {
         "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
-        "aws_secret_access_key": os.getenv("AWS_SECRET_KEY"),
+        "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
         "region_name": os.getenv("AWS_DEFAULT_REGION"),
     },
     "MAILGUN_API_KEY": os.getenv("MAILGUN_API_KEY"),
