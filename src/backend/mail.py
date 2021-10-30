a85cd4d4d5839d73a5ca0e3b473cedac7df2796a
thebeanogamer <thebeanogamer@gmail.com>
2021-10-30 22:06:46 +0100

    Swap to `EmailMultiAlternatives`

--- src/backend/mail.py
+++ src/backend/mail.py
@@ -1,8 +1,11 @@
 from django.template.loader import render_to_string
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+
 
 def send_email(send_to: str, subject_line: str, template_name: str, **template_details) -> None:
     """Wrapper function around `django.core.mail.send_mail` to simplify use of templates."""
-    text = render_to_string(template_name + ".txt", template_details)
-    html = render_to_string(template_name + ".html", template_details)
-    send_mail(subject_line, text, None, [send_to], html_message=html)
+    msg = EmailMultiAlternatives(
+        subject_line, render_to_string(template_name + ".txt", template_details), None, [send_to]
+    )
+    msg.attach_alternative(render_to_string(template_name + ".html", template_details), "text/html")
+    msg.send()
