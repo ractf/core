089bfa3aa41f67ea4b6b13c9eaa03143d275d1c7
thebeanogamer <thebeanogamer@gmail.com>
2021-10-31 17:56:53 +0000

    Bring back the image in emails

--- src/backend/mail.py
+++ src/backend/mail.py
@@ -1,5 +1,8 @@
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
+from anymail.message import attach_inline_image_file
+from django.conf import settings
+from os import path
 
 
 def send_email(send_to: str, subject_line: str, template_name: str, **template_details) -> None:
@@ -7,5 +10,7 @@ def send_email(send_to: str, subject_line: str, template_name: str, **template_d
     msg = EmailMultiAlternatives(
         subject_line, render_to_string(template_name + ".txt", template_details), None, [send_to]
     )
+    cid = attach_inline_image_file(msg, path.join(settings.BASE_DIR, "logo.png"))
+    template_details |= {"logo": cid}
     msg.attach_alternative(render_to_string(template_name + ".html", template_details), "text/html")
     msg.send()
