59d5d9b6970a62c3c20a8a9459ef86bbc2ec56c9
thebeanogamer <thebeanogamer@gmail.com>
2022-01-16 18:12:15 +0000

    Always load the mail view
    Dynamically reloading urlconf in tests is just too hard

--- src/admin/urls.py
+++ src/admin/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
@@ -8,7 +7,5 @@ router = DefaultRouter()
 
 urlpatterns = [
     path("self_check/", views.SelfCheckView.as_view(), name="self-check"),
+    path("list/", views.mail_list, name="mail-list"),
 ]
-
-if settings.EMAIL_BACKEND == "anymail.backends.test.EmailBackend":
-    urlpatterns += [path("list/", views.mail_list, name="mail-list"), ]
