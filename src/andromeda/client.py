4fab5b1ae9f592add93f687a0ca84fc1660afffc
Jeremiah Boby <mail@jeremiahboby.me>
2022-02-26 21:49:07 +0100

    Add docstrings for the andromeda app

--- src/andromeda/client.py
+++ src/andromeda/client.py
@@ -1,3 +1,5 @@
+"""A simple andromeda API client."""
+
 from uuid import UUID
 
 import requests
@@ -8,6 +10,7 @@ from backend.exceptions import FormattedException
 
 
 def post(path, **kwargs):
+    """Send a post request to andromeda."""
     response = requests.post(
         f"{settings.ANDROMEDA_URL}/{path}",
         headers={"Authorization": settings.ANDROMEDA_API_KEY},
@@ -20,6 +23,7 @@ def post(path, **kwargs):
 
 
 def get(path, **kwargs):
+    """Send a get request to andromeda."""
     response = requests.get(
         f"{settings.ANDROMEDA_URL}/{path}",
         headers={"Authorization": settings.ANDROMEDA_API_KEY},
@@ -32,18 +36,22 @@ def get(path, **kwargs):
 
 
 def get_instance(user_id, job_id):
+    """Get a challenge instance of a given job id for a user."""
     return post("", json={"user": str(user_id), "job": job_id})
 
 
 def request_reset(user_id, job_id):
+    """Reset a challenge instance of a given job id for a user."""
     return post("reset", json={"user": str(user_id), "job": job_id})
 
 
 def list_jobs():
+    """Get a list of all jobs running on the andromeda host."""
     return get("jobs")
 
 
 def restart_job(job_id):
+    """Restarts a job with a given uuid."""
     try:
         UUID(job_id)
     except ValueError:
@@ -52,12 +60,15 @@ def restart_job(job_id):
 
 
 def list_instances():
+    """List all the instances of challenges on the andromeda host."""
     return get("instances")
 
 
 def sysinfo():
+    """Get the current system info of the andromeda host."""
     return get("sysinfo")
 
 
 def submit_job(job_spec):
+    """Submit a job to the andromeda host."""
     return post("job/submit", json=job_spec)
